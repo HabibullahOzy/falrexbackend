47a6049dc2b30ae93534d9d20e9800f55de9ad47
HabibullahOzy <md.habibullah7520@gmail.com>
2026-05-17 22:59:20 +0600

    updatedur

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const server = http.createServer(app);
 // ── Socket.IO ──────────────────────────────────────────────────────────────
 const io = new Server(server, {
   cors: {
-    origin:      process.env.CLIENT_URL || "https://falrex.com" || 'https://www.falrex.com',
+    origin: '*',
     credentials: true,
     methods:     ["GET", "POST"],
   },
